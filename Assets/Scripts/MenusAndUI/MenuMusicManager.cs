75801ab8eb1963f96f68b92551f5dcafa4923494
Dona Runi <asalage.dhna@gmail.com>
2025-06-04 17:09:55 +1200

    Added comments to MenuMusicManager.cs and cleaned up spacing of script

--- Assets/Scripts/MenusAndUI/MenuMusicManager.cs
+++ Assets/Scripts/MenusAndUI/MenuMusicManager.cs
@@ -1,72 +1,66 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuMusicManager : MonoBehaviour
-{
+public class MenuMusicManager : MonoBehaviour{
     public static MenuMusicManager instance;
     private AudioSource audioSource;
-    private int previousSceneIndex = -1; // store last loaded scene index
+    // store last loaded scene index to track scene transitions
+    private int previousSceneIndex = -1; 
 
-    void Awake()
-    {
-        if (instance == null)
+    void Awake() 
+    {   //this makes sure only 1 instance of the script exists at a time
+        if (instance == null) 
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject); //object stays alive betwenn scenes
             audioSource = GetComponent<AudioSource>();
-            audioSource.loop = true;
+            audioSource.loop = true; 
             audioSource.Play();
         }
-        else
-        {
+        else{
             Destroy(gameObject);
         }
     }
-
-    void OnEnable()
+    void OnEnable() //tracks scene changes
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    void OnDisable()
+    void OnDisable() //stops tracking when object is destroyed
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        int[] menuScenes = { 0, 1, 2, 4 };
+        int[] menuScenes = { 0, 1, 2, 4 }; //scenes that play menu music
 
-        // If coming from scene 5 to scene 0, restart the music
-        if (previousSceneIndex == 5 && scene.buildIndex == 0)
-        {
+        //restarts menu music when transitioning from softdev in-game scene back to main menu
+        if (previousSceneIndex == 5 && scene.buildIndex == 0) {
             audioSource.time = 0f; // reset to start
             audioSource.Play();
         }
 
-        // If coming from scene 3 to scene 0, restart the music
-        else if (previousSceneIndex == 3 && scene.buildIndex == 0)
-        {
+        //restarts menu music when transitioning from cybersec in-game scene back to main menu
+        else if (previousSceneIndex == 3 && scene.buildIndex == 0){
             audioSource.time = 0f; // reset to start
             audioSource.Play();
         }
-
-        if (System.Array.Exists(menuScenes, index => index == scene.buildIndex))
-        {
+        //if the current scene is a scene that plays menu music, makes sure music is playing
+        if (System.Array.Exists(menuScenes, index => index == scene.buildIndex)){
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
         }
-        else
-        {
+        else{ //stops menu music in scenes that are not menu screens
             if (audioSource.isPlaying)
             {
                 audioSource.Pause();
             }
         }
 
-        previousSceneIndex = scene.buildIndex; // update last scene index
+        previousSceneIndex = scene.buildIndex; // updates the scene index
     }
 }
 
