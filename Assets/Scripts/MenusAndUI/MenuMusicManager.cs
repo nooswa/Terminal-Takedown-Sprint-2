2565d090e5188fc1e3f660e27ac66e36676002ec
Dona Runi <asalage.dhna@gmail.com>
2025-06-09 17:38:24 +1200

    Updated MenuMusicManager script, based on code review comments

--- Assets/Scripts/MenusAndUI/MenuMusicManager.cs
+++ Assets/Scripts/MenusAndUI/MenuMusicManager.cs
@@ -1,66 +1,91 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuMusicManager : MonoBehaviour{
+public class MenuMusicManager : MonoBehaviour
+{
     public static MenuMusicManager instance;
     private AudioSource audioSource;
     // store last loaded scene index to track scene transitions
     private int previousSceneIndex = -1; 
 
+    // scene index constants
+    private const int MAIN_MENU          = 0;
+    private const int CLASS_SELECTION    = 1;
+    private const int CYBERSEC_CHECKLIST = 2;
+    private const int SOFTDEV_CHECKLIST  = 4;
+    private const int CYBERSEC_GAME      = 3;
+    private const int SOFTDEV_GAME       = 5;
+
+    // scenes that play menu music
+    private readonly int[] menuScenes = {
+        MAIN_MENU,
+        CLASS_SELECTION,
+        CYBERSEC_CHECKLIST,
+        SOFTDEV_CHECKLIST
+    };
+
     void Awake() 
-    {   //this makes sure only 1 instance of the script exists at a time
+    {   // this makes sure only 1 instance of the script exists at a time
         if (instance == null) 
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); //object stays alive betwenn scenes
+            DontDestroyOnLoad(gameObject); // object stays alive between scenes
             audioSource = GetComponent<AudioSource>();
             audioSource.loop = true; 
             audioSource.Play();
         }
-        else{
+        else
+        {
             Destroy(gameObject);
         }
     }
-    void OnEnable() //tracks scene changes
+
+    void OnEnable() // tracks scene changes
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    void OnDisable() //stops tracking when object is destroyed
+    void OnDisable() // stops tracking when object is destroyed
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        int[] menuScenes = { 0, 1, 2, 4 }; //scenes that play menu music
+        int currentScene = scene.buildIndex;
 
-        //restarts menu music when transitioning from softdev in-game scene back to main menu
-        if (previousSceneIndex == 5 && scene.buildIndex == 0) {
-            audioSource.time = 0f; // reset to start
+        // restart music when returning to main menu from any game scene
+        if (currentScene == MAIN_MENU &&
+            (previousSceneIndex == CYBERSEC_GAME || previousSceneIndex == SOFTDEV_GAME))
+        {
+            restartMusic();
+        }
+
+        // play or pause depending on whether this is a menu scene
+        if (System.Array.Exists(menuScenes, i => i == currentScene))
+            ensureMusicPlaying();
+        else
+            pauseMusic();
+
+        previousSceneIndex = currentScene; // updates the scene index
+    }
+
+    private void restartMusic()
+    {
+        audioSource.time = 0f;
+        audioSource.Play();
+    }
+
+    private void ensureMusicPlaying()
+    {
+        if (!audioSource.isPlaying)
             audioSource.Play();
-        }
+    }
 
-        //restarts menu music when transitioning from cybersec in-game scene back to main menu
-        else if (previousSceneIndex == 3 && scene.buildIndex == 0){
-            audioSource.time = 0f; // reset to start
-            audioSource.Play();
-        }
-        //if the current scene is a scene that plays menu music, makes sure music is playing
-        if (System.Array.Exists(menuScenes, index => index == scene.buildIndex)){
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-        }
-        else{ //stops menu music in scenes that are not menu screens
-            if (audioSource.isPlaying)
-            {
-                audioSource.Pause();
-            }
-        }
-
-        previousSceneIndex = scene.buildIndex; // updates the scene index
+    private void pauseMusic()
+    {
+        if (audioSource.isPlaying)
+            audioSource.Pause();
     }
 }
 
